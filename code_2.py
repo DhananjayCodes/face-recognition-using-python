6da01ac7a986dcab97748c3c71b41d3b82492bc3
dhananjay <dhananjay8645@gamil.com>
2025-11-14 15:06:04 +0530

    the face recognition module is working proper although its deleting the data after exiting basically i have not created a database for it which i will do later

---
+++ code_2.py
@@ -0,0 +1,3 @@
+a=1
+b=0
+for x in range(2,4)
---
+++ study.py
@@ -0,0 +1,8 @@
+import pyjokes
+print("printing jokes...")
+# thin print jokes
+joke = pyjokes.get_joke('en', 'neutral')
+print(joke)
+"""
+so thanks that was my program
+"""
